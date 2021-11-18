b878efa9c451771733af317ae64663c7af6f0210
Hadrien G <knights_of_ni@gmx.com>
2021-11-18 19:41:35 +0100

    Switch to new architecture where display thread does the FFT

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,10 @@
 use anyhow::Result;
 use jack::{AudioIn, Control, Frames, NotificationHandler, Port, ProcessHandler, ProcessScope};
 use log::{debug, error, info, warn};
-use realfft::{num_complex::Complex, RealFftPlanner, RealToComplex};
-use rt_history::RTHistory;
-use std::{
-    panic::{catch_unwind, AssertUnwindSafe, UnwindSafe},
-    sync::{
-        atomic::{AtomicBool, Ordering},
-        Arc,
-    },
-};
+use realfft::RealFftPlanner;
+use rt_history::{Overrun, RTHistory};
+use std::panic::{catch_unwind, AssertUnwindSafe, UnwindSafe};
 use structopt::StructOpt;
-use triple_buffer::TripleBuffer;
 
 /// Remove DC offsets before computing Fourier transform
 const REMOVE_DC: bool = true;
@@ -73,35 +66,8 @@ struct ProcessState {
     /// Port which input data is coming from
     input_port: Port<AudioIn>,
 
-    /// Last observed samples from input_port, in a ring buffer layout.
-    last_points: Box<[f32]>,
-
-    /// Index at which we're currently writing in the last_points ring buffer
-    last_point_idx: usize,
-
-    /// FFT implementation
-    fft: Arc<dyn RealToComplex<f32>>,
-
-    /// FFT input buffer, will be overwritten
-    fft_input: Box<[f32]>,
-
-    /// FFT output buffer, will hold final coefficients
-    fft_output: Box<[Complex<f32>]>,
-
-    /// FFT scratch space, will be overwritten
-    fft_scratch: Box<[Complex<f32>]>,
-
-    /// Triple buffer to send FFT amplitudes to the main thread
-    fft_amps_in: triple_buffer::Input<Box<[f32]>>,
-
-    /// Number of audio periods to go through before computing an FFT
-    fft_period: usize,
-
-    /// Number of audio periods that we went through so far
-    curr_period: usize,
-
-    /// Truth that the display thread is ready to accept input
-    display_ready: Arc<AtomicBool>,
+    /// Output location to which audio frames are sent
+    output_hist: rt_history::Input<f32>,
 }
 
 impl ProcessHandler for ProcessState {
@@ -110,63 +76,9 @@ impl ProcessHandler for ProcessState {
         // back if Control::Quit is returned and the state is not accessible
         // after the thread has exited.
         handle_panics(AssertUnwindSafe(|| {
-            // Collect new audio data from JACK into our FFT input ring buffer
-            let new_data = self.input_port.as_slice(process_scope);
-            let (new_points, old_points) = self.last_points.split_at_mut(self.last_point_idx);
-            for (src, target) in new_data
-                .iter()
-                .zip(old_points.iter_mut().chain(new_points.iter_mut()))
-            {
-                *target = *src;
-            }
-            self.last_point_idx = (self.last_point_idx + new_data.len()) % self.last_points.len();
-
-            // Compute a new FFT if it's time to do so
-            self.curr_period += 1;
-            if self.curr_period >= self.fft_period {
-                // Linearize recent signal history
-                let (new_points, old_points) = self.last_points.split_at_mut(self.last_point_idx);
-                let (old_target, new_target) = self.fft_input.split_at_mut(old_points.len());
-                old_target.copy_from_slice(old_points);
-                new_target.copy_from_slice(new_points);
-
-                // Remove DC offset
-                if REMOVE_DC {
-                    let average = self.fft_input.iter().sum::<f32>() / self.fft_input.len() as f32;
-                    self.fft_input.iter_mut().for_each(|elem| *elem -= average);
-                }
-
-                // Compute FFT
-                let result = self.fft.process_with_scratch(
-                    &mut self.fft_input[..],
-                    &mut self.fft_output[..],
-                    &mut self.fft_scratch[..],
-                );
-                if let Err(err) = result {
-                    error!("Failed to compute FFT: {}", err);
-                    return Control::Quit;
-                }
-
-                // Normalize amplitudes, convert to dBm, and send the result out
-                let amps = self.fft_amps_in.input_buffer();
-                let norm_sqr = 1.0 / self.fft_input.len() as f32;
-                for (coeff, amp) in self.fft_output.iter().zip(amps.iter_mut()) {
-                    *amp = 10.0 * (coeff.norm_sqr() * norm_sqr).log10();
-                }
-                let overwrite = self.fft_amps_in.publish();
-
-                // The triple buffer tells us if we did overwrite an unread FFT, and
-                // we use that to probe the downstream readout rate and adjust our
-                // own FFT computation rate accordingly.
-                if overwrite && self.display_ready.load(Ordering::Relaxed) {
-                    // FIXME: Make sure this only triggers after client started
-                    //        receiving data, via an atomic.
-                    self.fft_period += 1;
-                } else {
-                    self.fft_period = self.fft_period.saturating_sub(1);
-                }
-                self.curr_period = 0;
-            }
+            // Forward new audio data from JACK into our history ring buffer
+            self.output_hist
+                .write(self.input_port.as_slice(process_scope));
             Control::Continue
         }))
     }
@@ -225,44 +137,74 @@ fn main() -> Result<()> {
         fft_len
     );
 
-    // Prepare for the FFT computation
-    let mut fft_planner = RealFftPlanner::<f32>::new();
-    let fft = fft_planner.plan_fft_forward(fft_len);
-    let fft_input = vec![0.; fft_len].into_boxed_slice();
-    let fft_output = fft.make_output_vec().into_boxed_slice();
-    let fft_scratch = fft.make_scratch_vec().into_boxed_slice();
+    // Set up a communication channel with the audio thread
+    let (hist_input, hist_output) = RTHistory::new(3 * fft_len).split();
 
-    // Prepare triple buffer for sending FFTs to the main thread
-    let fft_amps = TripleBuffer::new(vec![0.; fft_output.len()].into_boxed_slice());
-    let (fft_amps_in, mut fft_amps_out) = fft_amps.split();
-
-    // Start processing audio (FIXME: should be done once downstream chain is ready.
-    let display_ready = Arc::new(AtomicBool::new(false));
+    // Start recording audio
     let notification_handler = NotificationState { sample_rate };
     let process_handler = ProcessState {
         buffer_size: jack_client.buffer_size() as _,
         input_port,
-        last_points: fft_input.clone(),
-        last_point_idx: 0,
-        fft,
-        fft_input,
-        fft_output,
-        fft_scratch,
-        fft_amps_in,
-        fft_period: 1,
-        curr_period: 0,
-        display_ready: display_ready.clone(),
+        output_hist: hist_input,
     };
     let _jack_client = jack_client.activate_async(notification_handler, process_handler)?;
 
-    // HACK: Sleep a bit to see if audio thread is stable
-    display_ready.store(true, Ordering::Relaxed);
+    // Prepare for the FFT computation
+    let mut fft_planner = RealFftPlanner::<f32>::new();
+    let fft = fft_planner.plan_fft_forward(fft_len);
+    let mut fft_input = fft.make_input_vec().into_boxed_slice();
+    let mut fft_output = fft.make_output_vec().into_boxed_slice();
+    let mut fft_scratch = fft.make_scratch_vec().into_boxed_slice();
+    let mut fft_amps = vec![0.0; fft_output.len()].into_boxed_slice();
+
+    // Start computing some FFTs
+    let mut last_clock = 0;
     for _ in 0..1000 {
-        std::thread::sleep(std::time::Duration::from_millis(17));
-        if !fft_amps_out.update() {
-            warn!("Display thread got a stale FFT. Audio thread is overloaded or buffer size is too high.");
+        // DEBUG: Simulate vertical synchronization
+        std::thread::sleep(std::time::Duration::from_millis(7));
+
+        // Fetch some FFT history, check for overruns and underruns
+        last_clock = match hist_output.read(&mut fft_input[..]) {
+            Ok(clock) => {
+                if clock == last_clock {
+                    warn!("Buffer underrun! (No new data from audio thread)");
+                }
+                clock
+            }
+            Err(Overrun {
+                clock,
+                excess_entries,
+            }) => {
+                error!(
+                    "Buffer overrun! (Audio thread overwrote {} samples)",
+                    excess_entries
+                );
+                clock
+            }
+        };
+
+        // Remove DC offset
+        if REMOVE_DC {
+            let average = fft_input.iter().sum::<f32>() / fft_len as f32;
+            fft_input.iter_mut().for_each(|elem| *elem -= average);
         }
-        // eprintln!("Current FFT is {:?}", fft_amps_out.output_buffer());
+
+        // Compute FFT
+        fft.process_with_scratch(
+            &mut fft_input[..],
+            &mut fft_output[..],
+            &mut fft_scratch[..],
+        )
+        .expect("Failed to compute FFT");
+
+        // Normalize amplitudes, convert to dBm, and send the result out
+        let norm_sqr = 1.0 / fft_input.len() as f32;
+        for (coeff, amp) in fft_output.iter().zip(fft_amps.iter_mut()) {
+            *amp = 10.0 * (coeff.norm_sqr() * norm_sqr).log10();
+        }
+
+        // TODO: Display the FFT
+        // eprintln!("Current FFT is {:?}", fft_amps);
     }
 
     Ok(())
