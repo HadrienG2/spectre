4fe52ccb576a1650c5a51c30dac9815933280576
Hadrien Grasland <hadrien.grasland@gmx.fr>
2021-12-22 20:33:39 +0100

    Try an optimization suggestion, re-do the tuning with optimal compiler configuration

--- src/simd.rs
+++ src/simd.rs
@@ -46,29 +46,42 @@ impl AddAssign for SimdF32 {
 
 /// Sum an array of f32s
 pub fn sum_f32(input: &[f32]) -> f32 {
+    // Tuned on a Zen 2 CPU (AMD Ryzen 7 4800H) with Rust 1.57.0
     if cfg!(target_feature = "avx") {
         if input.len() < 16 {
             input.iter().sum::<f32>()
-        } else if input.len() < 256 {
-            sum_f32_impl::<1>(input)
+        } else if input.len() < 64 {
+            sum_f32_impl::<1, 1>(input)
+        } else if input.len() < 1024 {
+            sum_f32_impl::<4, 1>(input)
         } else {
-            sum_f32_impl::<2>(input)
+            sum_f32_impl::<8, 1>(input)
         }
     } else {
-        if input.len() < 32 {
+        if input.len() < 16 {
             input.iter().sum::<f32>()
         } else if input.len() < 256 {
-            sum_f32_impl::<1>(input)
-        } else if input.len() < 1024 {
-            sum_f32_impl::<2>(input)
+            sum_f32_impl::<1, 1>(input)
         } else {
-            sum_f32_impl::<4>(input)
+            // NOTE: This surprising optimal width originates from the fact that
+            //       rustc generates surprisingly bad code for 2xN, 4xN and 8xN.
+            sum_f32_impl::<16, 1>(input)
         }
     }
 }
 
-/// Implementation of sum_f32 with tunable (power-of-2) SIMD op concurrency
-fn sum_f32_impl<const CONCURRENCY: usize>(input: &[f32]) -> f32 {
+/// SIMD implementation of sum_f32 with tunable optimization parameters
+///
+/// CONCURRENCY controls the number of independent SIMD instruction streams.
+/// These can be leveraged by the CPU's superscalar backend for better
+/// performance on large inputs, at the cost of reducing performance on small
+/// inputs. This parameter must be a power of 2.
+///
+/// BLOCK_SIZE makes sure that independent instruction streams access distant
+/// inputs, which can improve performance on CPUs where cache lines belonging
+/// to different banks / associativity sets can be fetched in parallel.
+///
+fn sum_f32_impl<const CONCURRENCY: usize, const BLOCK_SIZE: usize>(input: &[f32]) -> f32 {
     // Reinterprete input as a slice of aligned SIMD vectors + some extra floats
     let (peel, vectors, tail) = unsafe { input.align_to::<SimdF32>() };
 
@@ -76,15 +89,17 @@ fn sum_f32_impl<const CONCURRENCY: usize>(input: &[f32]) -> f32 {
     let sum = |slice: &[f32]| slice.iter().sum::<f32>();
     let peel_sum = sum(peel);
 
-    // Chunk the aligned SIMD data according to desired concurrency
-    let chunks = vectors.chunks_exact(CONCURRENCY);
+    // Chunk the aligned SIMD data according to desired concurrency & block size
+    let chunks = vectors.chunks_exact(CONCURRENCY * BLOCK_SIZE);
     let remainder = chunks.remainder();
 
     // Perform concurrent SIMD accumulation
     let mut accumulators = [SimdF32::default(); CONCURRENCY];
     for chunk in chunks {
-        for (accumulator, &vector) in accumulators.iter_mut().zip(chunk) {
-            *accumulator += vector;
+        for vec in 0..BLOCK_SIZE {
+            for acc in 0..CONCURRENCY {
+                accumulators[acc] += chunk[acc * BLOCK_SIZE + vec];
+            }
         }
     }
 
