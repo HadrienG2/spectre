3e5ab98d4bceb2e7769a80a4e53dff518a8ba503
Hadrien Grasland <hadrien.grasland@gmx.fr>
2021-12-24 11:19:01 +0100

    Check that the interpolator works

--- src/math/mod.rs
+++ src/math/mod.rs
@@ -12,6 +12,7 @@ pub fn interpolate_c32(
     stride: usize,
 ) -> impl Iterator<Item = Complex<f32>> + '_ {
     assert!(input.len() > 0);
+    assert!(stride > 0);
     let inv_stride = 1.0 / stride as f32;
     input
         .windows(2)
@@ -24,4 +25,46 @@ pub fn interpolate_c32(
             })
         })
         .chain(std::iter::repeat(input.last().unwrap().clone()).take(stride))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use quickcheck::TestResult;
+    use quickcheck_macros::quickcheck;
+
+    #[quickcheck]
+    fn interpolate_c32(input: Vec<f32>, stride: usize) -> TestResult {
+        // Ignore silly configurations and keep output below 4MB
+        if input.len() < 2
+            || input.iter().any(|x| !x.is_finite())
+            || stride == 0
+            || input.len().saturating_mul(stride) > 1_000_000
+        {
+            return TestResult::discard();
+        }
+
+        // Turn real input into complex input
+        let input = input
+            .windows(2)
+            .map(|pair| Complex::new(pair[0], pair[1]))
+            .collect::<Box<[_]>>();
+
+        // Perform the interpolation
+        let output = super::interpolate_c32(&input[..], stride).collect::<Box<[_]>>();
+
+        // Check that the interpolant has the right length
+        assert_eq!(output.len(), stride * input.len());
+
+        // Check that the interpolant has the right values
+        for (idx, &output) in output.iter().enumerate() {
+            let left_idx = idx / stride;
+            let right_idx = (left_idx + 1).min(input.len() - 1);
+            let left = input[left_idx];
+            let right = input[right_idx];
+            let weight = (idx % stride) as f32 / stride as f32;
+            assert_eq!(output, (1.0 - weight) * left + weight * right);
+        }
+        TestResult::passed()
+    }
 }
